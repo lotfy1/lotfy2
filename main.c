e0b41ff53c95bc96d25f330bc5cc69643f7e6dea
lotfy1 <ahmed.lotfy614@gmail.com>
2020-12-09 21:10:36 +0200

    your comment

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+
+
+
+int main()
+{ int x = 10, y =5 ;
+printf("%d\n", add(x,y ));
+printf("%d\n", sub (x,y ));
+printf("%d\n", mult (x,y));
+swap(x,y);
+
+
+
+    return 0;
+}
